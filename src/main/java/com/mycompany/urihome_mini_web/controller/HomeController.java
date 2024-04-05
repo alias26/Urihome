a99228677160cb20114c52b608d91ad41610f255
alias26 <khmjuhee979@gmail.com>
2024-04-05 15:24:53 +0900

    Chore: Add controller package and change HomeController location

--- src/main/java/com/mycompany/urihome_mini_web/controller/HomeController.java
+++ src/main/java/com/mycompany/urihome_mini_web/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.mycompany.urihome_mini_web;
+package com.mycompany.urihome_mini_web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
