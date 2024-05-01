4fccc8d0840191d2416ef0152d26f2fe11c49b7c
alias26 <khmjuhee979@gmail.com>
2024-05-01 22:46:35 +0900

    Feat: Add MultipartFile field

--- src/main/java/com/mycompany/urihome_mini_web/dto/Product.java
+++ src/main/java/com/mycompany/urihome_mini_web/dto/Product.java
@@ -2,19 +2,23 @@ package com.mycompany.urihome_mini_web.dto;
 
 import java.util.Date;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import lombok.Data;
 
 @Data
 public class Product {
-	public String pid;
-	public String pname;
-	public String pthumbnail;
-	public String pbody;
-	public int pprice;
-	public int pstock;
-	public Date pdate;
-	public int psellAmount;
-	public int psales;
-	public int productHitcount;
-
+	private String pid;
+	private String pname;
+	private String pthumbnail;
+	private String pbody;
+	private int pprice;
+	private int pstock;
+	private Date pdate;
+	private int psellAmount;
+	private int psales;
+	private int productHitcount;
+	
+	public MultipartFile pthumbnailImage;
+	public MultipartFile pbodyImage;
 }
