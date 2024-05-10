9204246614e377514fd240bb39d9872ea0ea7cb2
김지혜 <ujubongbong@gmail.com>
2024-05-10 10:58:53 +0900

    Feat: Connect to DB to BoardProduct

--- src/main/java/com/mycompany/urihome_mini_web/dao/BoardProductDao.java
+++ src/main/java/com/mycompany/urihome_mini_web/dao/BoardProductDao.java
@@ -1,8 +1,18 @@
 package com.mycompany.urihome_mini_web.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.mycompany.urihome_mini_web.dto.BoardProduct;
+import com.mycompany.urihome_mini_web.dto.Pager;
 
 @Mapper
 public interface BoardProductDao {
-
+	
+	public int insert(BoardProduct boardProduct);
+	public List<BoardProduct> selectByPage(Pager pager);
+	public int count();
+	public BoardProduct selectByBpno(int bpnumber);
+	
 }
--- src/main/java/com/mycompany/urihome_mini_web/dto/BoardProduct.java
+++ src/main/java/com/mycompany/urihome_mini_web/dto/BoardProduct.java
@@ -1,7 +1,7 @@
 package com.mycompany.urihome_mini_web.dto;
 
-import java.sql.Clob;
 import java.util.Date;
+import org.springframework.web.multipart.MultipartFile;
 
 import lombok.Data;
 
@@ -9,10 +9,14 @@ import lombok.Data;
 public class BoardProduct {
 	private int bpnumber;
 	private String bptitle;
-	private String bpwriter;
-	private String bpimage;
-	private Clob bpcontent;
+	private String mid;
+	private String bpcontent;
 	private Date bpdate;
+	private String bptype;
 	private String pid;
-	private String bptyle;
+	private String bpattachoname;
+	private String bpattachtype;
+	private byte[] bpattachdata;
+	
+	private MultipartFile bpattach;	// 큰 파일을 청크 단위로 쪼개서 효율적으로 파일 업로드
 }
