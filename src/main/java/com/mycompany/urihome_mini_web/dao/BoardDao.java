ae2ac9bea798728fac12517eed424f6fe3cbd45c
sonyo <sonyo@DESKTOP-5E9HHPP>
2024-05-10 09:54:50 +0900

    Fix: change BoardDao

--- src/main/java/com/mycompany/urihome_mini_web/dao/BoardDao.java
+++ src/main/java/com/mycompany/urihome_mini_web/dao/BoardDao.java
@@ -11,10 +11,13 @@ import com.mycompany.urihome_mini_web.dto.Pager;
 @Mapper
 public interface BoardDao {
 	public int insert(Board board);
-	public List<Board> selectByPage(Pager pager);
 	public int count();
-	public Board selectByBno();
-	public Board selectAttachData();
+	public List<Board> selectByPage(Pager pager);
+	public Board selectByBno(int bnumber);
+	public Board selectByPrevBno(int bnumber);
+	public Board selectByNextBno(int bnumber);
+	public Board selectAttachData(int bnumber);
 	public int update(Board board);
+	public int deleteByBno(int bnumber);
 
 }
